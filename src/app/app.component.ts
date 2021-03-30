380d345cd8a87f67231f219e5ed083b8cc84ce2d
Chester404 <chestermawuli@gmail.com>
2021-03-30 11:35:47 +0000

    ready fro deployment

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 't11-frontend';
+  name = "chester"
+  confirm = "confirm"
+
+ 
+}
